11cbd9fb2e6c3db521e6aa6694f636d510c019d8
softAdd <soft.snake867@gmail.com>
2019-11-05 22:01:33 +0500

    express http server

--- backend/node/server.js
+++ backend/node/server.js
@@ -1,1 +1,10 @@
 const http = require('http')
+const express = require('express')
+const app = express()
+const server = http.createServer(app)
+
+app.get('/', (req, res) => {
+    res.send('Hello from express')
+})
+
+server.listen(process.env.APP_PORT || 3000, process.env.APP_IP || '127.0.0.1', () => console.log('Server started...'))
