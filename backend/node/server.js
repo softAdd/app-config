f3a5d91e393536d53e7eb6256157225661b4e36e
softAdd <soft.snake867@gmail.com>
2019-12-12 22:02:11 +0500

    node

--- backend/node/server.js
+++ backend/node/server.js
@@ -3,8 +3,19 @@ const express = require('express')
 const app = express()
 const server = http.createServer(app)
 
+const { APP_PORT, APP_IP } = process.env
+
 app.get('/', (req, res) => {
     res.send('Hello from express')
 })
 
-server.listen(process.env.APP_PORT || 3000, process.env.APP_IP || '127.0.0.1', () => console.log('Server started...'))
+server.listen(APP_PORT || 3000, APP_IP || '127.0.0.1', () => console.log(`Server running at http://${APP_IP}:${APP_PORT}/`))
+
+process.on('SIGTERM', () => {
+    console.info('SIGTERM signal received.')
+    console.log('Closing http server.')
+    server.close(() => {
+        console.log('Http server closed.')
+        process.exit(0)
+    })
+})
