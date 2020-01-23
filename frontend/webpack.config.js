8c82c9e42a6382f44d2ddc0c3cc7968bb4fac113
softAdd <soft.snake867@gmail.com>
2020-01-23 19:07:27 +0500

    min webpack config

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -1,102 +1,20 @@
-// common modules
-const path = require('path');
-const requireContext = require('require-context');
-
-// plugins
-const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
-// const CopyPlugin = require('copy-webpack-plugin');
-const ImageminPlugin = require('imagemin-webpack-plugin').default;
-const autoprefixer = require('autoprefixer');
-
-// collections
-const css = requireContext(__dirname + '/src/css', false, /\.css$/);
-
-function importAll(files, path) {
-    return files.keys().map(file => {
-        return path + '/' + file
-    })
-}
+const path = require('path')
+const HTMLPlugin = require('html-webpack-plugin')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
 
 module.exports = {
-    mode: 'production',
-    entry: {
-        app: [
-            './js/index.js',
-            ...importAll(css, './css')
-        ]
-    },
+    entry: './src/app.js',
     output: {
-        filename: 'js/[name].js',
-        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.[chunkhash].js',
+        path: path.resolve(__dirname, 'dist')
     },
-    context: path.resolve(__dirname, 'src'),
     devServer: {
-        contentBase: path.join(__dirname, 'dist'),
-        compress: true,
-        port: 3000,
-        watchContentBase: true
-    },
-    module: {
-        rules: [
-            {
-                test: /\.js$/,
-                exclude: /(node_modules|bower_components)/,
-                use: {
-                    loader: 'babel-loader',
-                    options: {
-                        presets: ['@babel/preset-env']
-                    }
-                }
-            },
-            {
-                test: /\.css$/,
-                use: [
-                    {
-                        loader: MiniCssExtractPlugin.loader
-                    },
-                    {
-                        loader: 'css-loader'
-                    },
-                    {
-                        loader: 'postcss-loader',
-                        options: {
-                            plugins: [
-                                autoprefixer({
-                                    browsers:['ie >= 11', 'last 4 version']
-                                })
-                            ]
-                        }
-                    }
-                ],
-            },
-            {
-                test: /\.(png|jpe?g|gif|svg)$/i,
-                use: [
-                    {
-                        loader: 'file-loader',
-                        options: {
-                            name: '[path][name].[ext]',
-                        }
-                    }
-                ]
-            }
-        ]
+        port: 3000
     },
     plugins: [
-        new MiniCssExtractPlugin({
-            filename: 'css/[name].css',
-        })
-    ],
-    optimization: {
-        minimizer: [
-            new UglifyJsPlugin({
-                test: /\.js(\?.*)?$/i,
-                exclude: /(node_modules|bower_components)/
-            }),
-            new OptimizeCSSAssetsPlugin({}),
-            new ImageminPlugin({ test: /\.(jpe?g|png|gif|svg)$/i })
-        ]
-    }
-};
+        new HTMLPlugin({
+            template: './src/public/index.html'
+        }),
+        new CleanWebpackPlugin()
+    ]
+}
