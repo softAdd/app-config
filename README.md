71990e17fe7976343977ce90c0c2e25005e8d3b3
softAdd <soft.snake867@gmail.com>
2020-02-17 21:11:34 +0500

    rebuild

--- README.md
+++ README.md
@@ -1,11 +1,1 @@
-An example of a config for a full site setup using:
-1) Webpack - for building a project (frontend)
-2) NodeJS - for processing requests from frontend
-3) Nginx - for distributing static content and for proxying requests on NodeJS
-
-Запуск Nginx
-sudo service nginx start
-После какого-либо изменения в файле конфигурации, мы должны заставить процесс NGINX перечитать конфиг (без перезагрузки) такой командой:
-service nginx reload
-И наконец, мы можем проверить состояние процесса командой:
-service nginx status
+# Сборник базовых конфигов
---
+++ webpack/webpack.config.js
@@ -0,0 +1,95 @@
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+const CopyWebpackPlugin = require('copy-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const TerserWebpackPlugin = require('terser-webpack-plugin')
+const OptimizeCssAssetsWebpackPlugin = require('optimize-css-assets-webpack-plugin')
+
+const isDev = process.env.NODE_ENV === 'development'
+const isProd = !isDev
+
+const filename = ext => isDev ? `\`[name].${ext}\`` : `\`[name].[hash].${ext}\``
+
+const optimization = () => {
+    const config = {
+        splitChunks: {
+            chunks: 'all'
+        }
+    }
+
+    if (isProd) {
+        config.minimizer = [
+            new OptimizeCssAssetsWebpackPlugin(),
+            new TerserWebpackPlugin()
+        ]
+    }
+
+    return config
+}
+
+module.exports = {
+    context: path.resolve(__dirname, 'src'),
+    mode: 'development',
+    entry: {
+        main: 'path/to/entry-file.js',
+    },
+    output: {
+        filename: filename('js'),
+        path: path.resolve(__dirname, 'dist')
+    },
+    resolve: {
+        alias: {
+            '@models': path.resolve(__dirname, 'src/path-to-models'),
+            '@': path.resolve(__dirname, 'src')
+        }
+    },
+    optimization: optimization(),
+    devServer: {
+        port: 4200,
+        hot: isDev
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './path/to/index.html',
+            minify: {
+                collapseWhiteSpace: isProd
+            }
+        }),
+        new CleanWebpackPlugin(),
+        new CopyWebpackPlugin([
+            {
+                from: path.resolve(__dirname, 'src/favicon.ico'),
+                to: path.resolve(__dirname, 'dist/favicon.ico')
+            }
+        ]),
+        new MiniCssExtractPlugin({
+            filename: filename('css')
+        })
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: [
+                    {
+                        loader: MiniCssExtractPlugin.loader,
+                        options: {
+                            hmr: isDev,
+                            reloadAll: true
+                        }
+                    },
+                    'css-loader'
+                ]
+            },
+            {
+                test: /\.(png|jpg|svg|gif)$/,
+                use: ['file-loader']
+            },
+            {
+                test: /\.(ttf|woff|woff2|eot)$/,
+                use: ['file-loader']
+            }
+        ]
+    }
+}
